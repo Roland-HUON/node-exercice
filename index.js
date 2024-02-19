1d4e7167d37d6e505886f5673bcd55025052ff88
Roland HUON <huonroland91@gmail.com>
2024-02-19 10:01:18 +0100

    ✨ Feat : add name in array and res

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('Hello, world!')
+import express from "express";
+
+const app = express();
+const port = 5000;
+const agentsValorants = [
+    'Jett',
+    'Raze',
+    'Breach',
+    'Omen',
+    'Brimstone',
+    'Phoenix',
+    'Sage',
+    'Sova',
+    'Viper',
+    'Cypher',
+    'Reyna',
+    'Killjoy',
+    'Skye',
+    'Yoru',
+    'Astra',
+    'Kay/o',
+    'Chamber',
+    'Neon',
+    'Fade',
+    'Harbor',
+    'Gekko',
+    'Deadlock',
+    'Iso'
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello there !');
+})
+app.get('/agents', (req, res) => {
+    res.send(agentsValorants);
+})
+
+app.listen(5000, () =>{
+    console.log('Example app listening at http://localhost:'+ port);
+})
